a38f06c76547dbdfb619f70ca1b5f6ff87a6ba29
Sadi Barkın Şeber <sadibarkn@gmail.com>
2023-10-02 23:14:21 +0300

    Add project files.

---
+++ listTransfer/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace listTransfer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSagaCoklu = new System.Windows.Forms.Button();
+            this.btnSagaTekli = new System.Windows.Forms.Button();
+            this.btnSolaTekli = new System.Windows.Forms.Button();
+            this.btnSolaCoklu = new System.Windows.Forms.Button();
+            this.labelSag = new System.Windows.Forms.ListBox();
+            this.lboxSol = new System.Windows.Forms.ListBox();
+            this.txtVeriEkle = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSagaCoklu
+            // 
+            this.btnSagaCoklu.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnSagaCoklu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSagaCoklu.Location = new System.Drawing.Point(329, 181);
+            this.btnSagaCoklu.Name = "btnSagaCoklu";
+            this.btnSagaCoklu.Size = new System.Drawing.Size(53, 30);
+            this.btnSagaCoklu.TabIndex = 0;
+            this.btnSagaCoklu.Text = ">>";
+            this.btnSagaCoklu.UseVisualStyleBackColor = false;
+            this.btnSagaCoklu.Click += new System.EventHandler(this.btnSagaCoklu_Click);
+            // 
+            // btnSagaTekli
+            // 
+            this.btnSagaTekli.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnSagaTekli.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSagaTekli.Location = new System.Drawing.Point(329, 222);
+            this.btnSagaTekli.Name = "btnSagaTekli";
+            this.btnSagaTekli.Size = new System.Drawing.Size(53, 30);
+            this.btnSagaTekli.TabIndex = 1;
+            this.btnSagaTekli.Text = ">";
+            this.btnSagaTekli.UseVisualStyleBackColor = false;
+            this.btnSagaTekli.Click += new System.EventHandler(this.btnSagaTekli_Click);
+            // 
+            // btnSolaTekli
+            // 
+            this.btnSolaTekli.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnSolaTekli.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSolaTekli.Location = new System.Drawing.Point(329, 266);
+            this.btnSolaTekli.Name = "btnSolaTekli";
+            this.btnSolaTekli.Size = new System.Drawing.Size(53, 30);
+            this.btnSolaTekli.TabIndex = 2;
+            this.btnSolaTekli.Text = "<";
+            this.btnSolaTekli.UseVisualStyleBackColor = false;
+            this.btnSolaTekli.Click += new System.EventHandler(this.btnSolaTekli_Click);
+            // 
+            // btnSolaCoklu
+            // 
+            this.btnSolaCoklu.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnSolaCoklu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSolaCoklu.Location = new System.Drawing.Point(329, 307);
+            this.btnSolaCoklu.Name = "btnSolaCoklu";
+            this.btnSolaCoklu.Size = new System.Drawing.Size(53, 30);
+            this.btnSolaCoklu.TabIndex = 3;
+            this.btnSolaCoklu.Text = "<<";
+            this.btnSolaCoklu.UseVisualStyleBackColor = false;
+            this.btnSolaCoklu.Click += new System.EventHandler(this.btnSolaCoklu_Click);
+            // 
+            // labelSag
+            // 
+            this.labelSag.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSag.FormattingEnabled = true;
+            this.labelSag.ItemHeight = 20;
+            this.labelSag.Location = new System.Drawing.Point(413, 105);
+            this.labelSag.Name = "labelSag";
+            this.labelSag.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.labelSag.Size = new System.Drawing.Size(280, 324);
+            this.labelSag.TabIndex = 4;
+            // 
+            // lboxSol
+            // 
+            this.lboxSol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lboxSol.FormattingEnabled = true;
+            this.lboxSol.ItemHeight = 20;
+            this.lboxSol.Location = new System.Drawing.Point(26, 105);
+            this.lboxSol.Name = "lboxSol";
+            this.lboxSol.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lboxSol.Size = new System.Drawing.Size(273, 324);
+            this.lboxSol.TabIndex = 5;
+            // 
+            // txtVeriEkle
+            // 
+            this.txtVeriEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtVeriEkle.Location = new System.Drawing.Point(145, 30);
+            this.txtVeriEkle.Name = "txtVeriEkle";
+            this.txtVeriEkle.Size = new System.Drawing.Size(215, 26);
+            this.txtVeriEkle.TabIndex = 7;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.ForeColor = System.Drawing.Color.Black;
+            this.btnEkle.Location = new System.Drawing.Point(376, 30);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(94, 27);
+            this.btnEkle.TabIndex = 8;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(22, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Listeye Ekle :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(23, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Liste 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(410, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 18);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Liste 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(705, 441);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtVeriEkle);
+            this.Controls.Add(this.lboxSol);
+            this.Controls.Add(this.labelSag);
+            this.Controls.Add(this.btnSolaCoklu);
+            this.Controls.Add(this.btnSolaTekli);
+            this.Controls.Add(this.btnSagaTekli);
+            this.Controls.Add(this.btnSagaCoklu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSagaCoklu;
+        private System.Windows.Forms.Button btnSagaTekli;
+        private System.Windows.Forms.Button btnSolaTekli;
+        private System.Windows.Forms.Button btnSolaCoklu;
+        private System.Windows.Forms.ListBox labelSag;
+        private System.Windows.Forms.ListBox lboxSol;
+        private System.Windows.Forms.TextBox txtVeriEkle;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ listTransfer/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listTransfer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> liste = new List<string>();
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            lboxSol.Items.Add(txtVeriEkle.Text);
+            liste.Add(txtVeriEkle.Text);
+            txtVeriEkle.Clear();
+        }
+
+        private void btnSagaTekli_Click(object sender, EventArgs e)
+        {
+            if (lboxSol.SelectedIndices.Count == 0)
+            {
+                MessageBox.Show("Eleman Seçmediniz.");
+            }
+            else
+            {
+                for (int i = lboxSol.SelectedIndices.Count - 1; i >= 0; i--)
+                {
+
+                    labelSag.Items.Add(lboxSol.SelectedItems[i]);
+                    lboxSol.Items.Remove(lboxSol.SelectedItems[i]);
+                }
+            }
+
+
+        }
+
+        private void btnSolaTekli_Click(object sender, EventArgs e)
+        {
+            if (labelSag.SelectedIndices.Count == 0)
+            {
+                MessageBox.Show("Eleman Seçmediniz.");
+            }
+            else
+            {
+                for (int i = labelSag.SelectedIndices.Count - 1; i >= 0; i--)
+                {
+                    lboxSol.Items.Add(labelSag.SelectedItem);
+                    labelSag.Items.Remove(labelSag.SelectedItem);
+                }
+            }
+
+        }
+
+        private void btnSagaCoklu_Click(object sender, EventArgs e)
+        {
+            labelSag.Items.Clear();
+            labelSag.Items.AddRange(liste.ToArray());
+            lboxSol.Items.Clear();
+        }
+
+        private void btnSolaCoklu_Click(object sender, EventArgs e)
+        {
+            lboxSol.Items.Clear();
+            lboxSol.Items.AddRange(liste.ToArray());
+            labelSag.Items.Clear();
+        }
+
+    }
+}
